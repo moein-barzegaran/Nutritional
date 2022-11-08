b3a4cee193cebf9c5abfb5065d9ab5031a6105a9
Moein Barzegaran <barzegaranmoein@gmail.com>
2022-11-08 12:22:03 +0300

    Initial Commit

---
+++ Nutritional/NutritionalApp.swift
@@ -0,0 +1,17 @@
+//
+//  NutritionalApp.swift
+//  Nutritional
+//
+//  Created by Moein Barzegaran on 11/8/22.
+//
+
+import SwiftUI
+
+@main
+struct NutritionalApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
